78ed1dd9b4ad54251025e4158a4e10c1fe959cc1
Jeff Britton <britton.jeff@gmail.com>
2019-09-17 00:02:20 -0400

    Reworking portofolio home grid layout, adding zoom effect.

--- src/components/sections/PortfolioHome.js
+++ src/components/sections/PortfolioHome.js
@@ -7,12 +7,10 @@ const renderTile = ({path, link, linkSize, thumbnail}) => {
     const textSize = (linkSize === 'L')? 'label-large' : '';
     const linkClasses = 'label ' + textSize;
     return (
-        <div className="gallery-link-container">
-            <Link to={path} className="gallery-link">
-                <img src={`assets/tiles/${thumbnail}`}
-                     className="w3-image" />
-            </Link>
-        </div>
+        <Link to={path} className="portfolio-link">
+            <img src={`assets/tiles/${thumbnail}`}
+                 className="w3-image" />
+        </Link>
     );
 };
 
@@ -25,87 +23,59 @@ const PortfolioHome = () => {
                 <h1 className="project-header">Creative Direction & Design</h1>
             </div>
 
-            <div className="w3-row">
-                <div className="w3-col s6">
+            <div className="portfolio-grid">
+                <div className="grid-a">
                     {renderTile(portfolioData.omfBrand)}
                 </div>
-                <div className="w3-col s6">
+                <div className="grid-b">
                     {renderTile(portfolioData.omfWeb)}
                 </div>
-            </div>
-
-            <div className="w3-row">
-                <div className="w3-col s6">
+                <div className="grid-c">
                     {renderTile(portfolioData.omfEmail)}
                 </div>
-                <div className="w3-col s6">
+                <div className="grid-d">
                     {renderTile(portfolioData.omfMainStreet)}
                 </div>
-            </div>
-
-            <div className="w3-row">
-                <div className="w3-col s6">
+                <div className="grid-e">
                     {renderTile(portfolioData.brazilInnovation)}
                 </div>
-                <div className="w3-col s6">
-                    <div className="w3-row">
-                        <div className="w3-col s6">
-                            {renderTile(portfolioData.backToSchool)}
-                        </div>
-                        <div className="w3-col s6">
-                            {renderTile(portfolioData.hockeyWorld)}
-                        </div>
-                    </div>
-                    <div className="w3-row">
-                        <div className="w3-col s6">
-                            {renderTile(portfolioData.arris)}
-                        </div>
-                        <div className="w3-col s6">
-                            {renderTile(portfolioData.footwear)}
-                        </div>
-                    </div>
+                <div className="grid-f">
+                    {renderTile(portfolioData.backToSchool)}
                 </div>
-            </div>
-
-            <div className="w3-row">
-                <div className="w3-col s6">
+                <div className="grid-g">
+                    {renderTile(portfolioData.hockeyWorld)}
+                </div>
+                <div className="grid-h">
+                    {renderTile(portfolioData.arris)}
+                </div>
+                <div className="grid-i">
+                    {renderTile(portfolioData.footwear)}
+                </div>
+                <div className="grid-j">
                     {renderTile(portfolioData.cogenderUnderwear)}
                 </div>
-                <div className="w3-col s6">
+                <div className="grid-k">
                     {renderTile(portfolioData.iWill)}
                 </div>
-            </div>
-
-            <div className="w3-row">
-                <div className="w3-col s4">
-                    <div className="w3-row">
-                        <div className="w3-col s12">
-                            {renderTile(portfolioData.uniforms)}
-                        </div>
-                        <div className="w3-col s12">
-                            {renderTile(portfolioData.euSiteLaunch)}
-                        </div>
-                    </div>
+                <div className="grid-l">
+                    {renderTile(portfolioData.uniforms)}
                 </div>
-                <div className="w3-col s8">
+                <div className="grid-m">
+                    {renderTile(portfolioData.euSiteLaunch)}
+                </div>
+                <div className="grid-n">
                     {renderTile(portfolioData.americanGiftGuide)}
                 </div>
-            </div>
-
-            <div className="w3-row">
-                <div className="w3-col s6">
+                <div className="grid-o">
                     {renderTile(portfolioData.runWorld)}
                 </div>
-                <div className="w3-col s6">
+                <div className="grid-p">
                     {renderTile(portfolioData.productStyling)}
                 </div>
-            </div>
-
-            <div className="w3-row">
-                <div className="w3-col s6">
+                <div className="grid-q">
                     {renderTile(portfolioData.euGiftGuide)}
                 </div>
-                <div className="w3-col s6">
+                <div className="grid-r">
                     {renderTile(portfolioData.paypal)}
                 </div>
             </div>
